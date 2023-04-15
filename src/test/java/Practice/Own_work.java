ac8ce09f00126373adae227aa1ad5d453f363f99
Shoboy11 <130847544+Shoboy11@users.noreply.github.com>
2023-04-15 12:58:06 -0400

    new project something with prices

--- src/test/java/Practice/Own_work.java
+++ src/test/java/Practice/Own_work.java
@@ -6,6 +6,6 @@ import org.testng.annotations.Test;
 public class Own_work extends ReusableAnnotations {
     @Test
     public void myWork(){
-        System.out.println("Yeers");
+        driver.navigate().to("https://www.google.com");
     }//end of test case
 }//end of class
