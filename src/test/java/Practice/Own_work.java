9ae3cf107fca64a00bbe6e80235260a286eaab01
Shoboy11 <130847544+Shoboy11@users.noreply.github.com>
2023-04-15 12:59:43 -0400

    new project something with prices

--- src/test/java/Practice/Own_work.java
+++ src/test/java/Practice/Own_work.java
@@ -4,8 +4,8 @@ import ReusableLibraries.ReusableAnnotations;
 import org.testng.annotations.Test;
 
 public class Own_work extends ReusableAnnotations {
-    @Test
+    /*@Test
     public void myWork(){
-
+*/
     }
 }
